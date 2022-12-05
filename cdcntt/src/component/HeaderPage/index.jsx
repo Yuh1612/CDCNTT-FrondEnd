1e1303ca3d33bad927f6c721021474357f8edbd5
Huy Trần Phan <huytran161201@gmail.com>
2022-12-05 21:14:16 +0700

    update UI Sign In, Sign Up and Navbar

--- cdcntt/src/component/HeaderPage/index.jsx
+++ cdcntt/src/component/HeaderPage/index.jsx
@@ -1,45 +1,45 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link, NavLink } from 'react-router-dom';
-import {FcSearch} from 'react-icons/fc';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./headerPage.css";
+import logo from "../../assets/logo.jpg";
 function HeaderPage() {
   return (
-    
- <Navbar className='Navbar' bg="light" expand="lg" fixed='top'>
+    <Navbar className="Navbar" expand="lg" fixed="top">
       <Container fluid>
-        <Navbar.Brand href="/"><FcSearch/>Finding Teacher </Navbar.Brand>
+        <Navbar.Brand href="/" className="logo-container">
+          <img src={logo} alt="logo" className="logo" />
+          Finding Teacher{" "}
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            navbarScroll>
-            <NavLink to="/" className='nav-link  '>Home</NavLink>
-          </Nav>
+        <Navbar.Collapse id="navbarScroll" className="navbar-button">
           <Nav>
-          <Button className='btn' variant="outline-primary">
-            <NavLink to="signup" className='nav-link  ' id='signup'> Sign up
-            </NavLink>
-          </Button>
-          <Button className='btn' variant="outline-primary">
-            <NavLink to="login" className='nav-link ' id='login'> Log in
-            </NavLink>
-          </Button>
-          <Button className='btn' variant="outline-primary">
-            <NavLink to="infoGV" className='nav-link ' id='login'> Info Giang vien
-            </NavLink>
-          </Button>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-    </Navbar >
-   
-   
+            <Button className="btn" variant="outline-primary">
+              <NavLink to="signup" className="nav-link" id="signup">
+                {" "}
+                Sign up
+              </NavLink>
+            </Button>
+            <Button className="btn" variant="outline-primary">
+              <NavLink to="login" className="nav-link" id="login">
+                {" "}
+                Sign in
+              </NavLink>
+            </Button>
+            <Button className="btn" variant="outline-primary">
+              <NavLink to="infoGV" className="nav-link" id="login">
+                {" "}
+                Teacher
+              </NavLink>
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default HeaderPage
+export default HeaderPage;
