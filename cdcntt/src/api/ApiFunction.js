9d5e470a11c73151f85536d78e7a9f7f0ddfb6d2
Huy Trần Phan <huytran161201@gmail.com>
2022-12-12 20:52:40 +0700

    update api

--- cdcntt/src/api/ApiFunction.js
+++ cdcntt/src/api/ApiFunction.js
@@ -1,8 +1,62 @@
 import apiConfig from "./ApiConfig";
 
 const api = {
-  async getRaitings() {
-    return await apiConfig.get("/raitings");
+  async getRaitings(teacherId) {
+    return await apiConfig.get("/raitings", teacherId);
+  },
+  async createRaiting(teacherId, studentId, raiting, comment) {
+    return await apiConfig.post("/raiting", {
+      id_Teacher: teacherId,
+      id_Student: studentId,
+      raiting: raiting,
+      comment: comment,
+    });
+  },
+  async createTeacher(
+    username,
+    password,
+    firstname,
+    lastname,
+    photo,
+    phone,
+    address,
+    experience,
+    degree,
+    cerfiticate
+  ) {
+    return await apiConfig.post("/teacher", {
+      username: username,
+      password: password,
+      firstname: firstname,
+      lastname: lastname,
+      photo: photo,
+      phone: phone,
+      address: address,
+      experience: experience,
+      degree: degree,
+      cerfiticate: cerfiticate,
+    });
+  },
+  async createStudent(username, password, firstname, lastname, phone) {
+    return await apiConfig.post("/student", {
+      username: username,
+      password: password,
+      firstname: firstname,
+      lastname: lastname,
+      phone: phone,
+    });
+  },
+  async loginWithTeacher(username, password) {
+    return await apiConfig.post("/teacher/login", {
+      username: username,
+      password: password,
+    });
+  },
+  async loginWithStudent(username, password) {
+    return await apiConfig.post("/student/login", {
+      username: username,
+      password: password,
+    });
   },
 };
 
